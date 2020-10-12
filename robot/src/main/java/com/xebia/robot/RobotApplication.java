b673a0b09689f8f0e0624b783cd4d89ab58ca389
pavi93 <60815416+pavi93@users.noreply.github.com>
2020-10-12 17:17:32 +0530

    Update RobotApplication.java

--- robot/src/main/java/com/xebia/robot/RobotApplication.java
+++ robot/src/main/java/com/xebia/robot/RobotApplication.java
@@ -11,6 +11,7 @@ public class RobotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RobotApplication.class, args);
+		System.out.println("App started Successfully");
 	}
 	
 	@Bean
